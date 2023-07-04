8997b4d964cd8b0b7a186c6af05229d35f82f7f1
316de <316de@315-DE-8>
2023-07-04 09:41:34 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Garden
+    {
+        public string Owner { get; set; }
+        public string Adress { get; set; }
+
+        public List<Flower> Flowers = new List<Flower>();
+
+        public Garden(string own, string add)
+        {
+            Owner = own;
+            Adress = add;
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine("Информация о саде:\n" + $"\tВладелец: {Owner}\n" + $"\tАдрес: {Adress}\n\n" + $"Цветы сада:");
+            Flowers.Sort();
+            foreach (Flower flower in Flowers)
+            {
+                flower.Show();
+            }
+        }
+    }
+    public class Flower : IComparable
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Flower(string t, string des)
+        {
+            Title = t;
+            Description = des;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Информация о цветке:\n" + $"\tНазвание: {Title}\n" + $"\tВладелец: {Description}");
+        }
+        public int CompareTo(object obj)
+        {
+            return (this.Title + this.Description).CompareTo(Title + Description);
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Garden garden = new Garden("Запорожан Илья", "Староватутенский проезд");
+            
+            garden.Flowers.Add(new Flower($"Роза", "Вася"));
+            garden.Flowers.Add(new Flower($"Пионы", "Петя"));
+            garden.Flowers.Add(new Flower($"Бегония", "Вадим"));
+            garden.Flowers.Add(new Flower($"Колеус", "Вадим"));
+
+            garden.ShowAll();
+
+            Console.ReadKey();
+        }
+    }
+}
